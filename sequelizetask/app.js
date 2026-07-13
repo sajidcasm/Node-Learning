6d068132f98e8745d8eba2a80c579cd52651c58c
Md Sajid Ali <msajidali@trueigtech.com>
2026-07-13 17:07:17 +0530

    hardcoed get api and data addition !

--- sequelizetask/app.js
+++ sequelizetask/app.js
@@ -2,15 +2,20 @@ require("dotenv").config();
 console.log("test hello from appjs ");
 
 const express = require("express");
-const exphbs = require("express-handlebars");
+const { engine } = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
 
 const database = require('./database.js')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.ENV_PORT || 5000;
 
 const app = express();
+//  express handlebars 
+
+app.engine('handlebars',engine({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
 
 database.authenticate().then(()=>console.log("Database connected ")).catch(error=>console.log(error));
 app.get("/", (req,res)=>{
--- sequelizetask/routes/gigs.js
+++ sequelizetask/routes/gigs.js
@@ -1,26 +1,48 @@
-const express = require('express');
-const db = require('../database.js');
+const express = require("express");
+const db = require("../database.js");
 
-const modell = require('../Models/model.js');
+const modell = require("../Models/model.js");
 
 const router = express.Router();
 
-
-router.get('/', (req,res)=>{
-
-    modell.findAll().then((data)=>{
-        console.log(data);
-        res.sendStatus(200);
-    }).catch((error)=>{
-        console.log(error);
+router.get("/", (req, res) => {
+  modell
+    .findAll()
+    .then((data) => {
+      console.log(data);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-    
-})
+});
 
+router.get("/hardcode-add", (req, res) => {
+  const data = {
+    title: "luking for a react dev",
+    technologies: " react,js, nextjs, nodejs ",
+    budget: "$3000",
+    description: "lorem ipsum data is there xyz xyz xyz !!!",
+    contact_email: "ss@gmail.com",
+  };
 
-router.get('/nigs', (req,res)=>{
+  let { title, technologies, budget, description, contact_email } = data;
 
-    res.send("yo route of get gigs/nigs..............  ")
-})
+  modell.create({
+    title,
+    technologies,
+    budget,
+    description,
+    contact_email,
+  }).then((data)=>{
+    console.log('data');
+    // res.redirect('/gigs');
+    res.send('hardcoded data added by just calling get request ! ');
+
+  }).catch((error)=>{
+    console.log('error', error);
+
+  })
+});
 
 module.exports = router;
