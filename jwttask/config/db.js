18e29db96518f4f7ee9edf134c900ff02dd36e26
Md Sajid Ali <msajidali@trueigtech.com>
2026-07-16 14:38:23 +0530

    sequelize model add

--- jwttask/config/db.js
+++ jwttask/config/db.js
@@ -16,3 +16,16 @@ const sequelize = new Sequelize(
 );
 
 export default sequelize;
+
+// in app . js u gotta do this thing now 
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("✅ Database Connected Successfully");
+//   } catch (error) {
+//     console.error("❌ Database Connection Failed");
+//     console.error(error.message);
+//   }
+// };
+
+// connectDB();
---
+++ jwttask/models/User.js
@@ -0,0 +1,38 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Users = sequelize.define(
+  "Users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: false,
+  },
+);
+
+
+export default Users;
